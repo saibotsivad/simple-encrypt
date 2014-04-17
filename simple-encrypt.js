4125101ff905585bc02367ed8491e1469c49bfd9
Tobias Davis <tobias@davistobias.com>
2014-04-17 01:45:25 -0500

    Cleaned up, added tests. Feature complete.

--- simple-encrypt.js
+++ simple-encrypt.js
@@ -1,35 +1,80 @@
 'use strict'
 
 var ursa = require('ursa')
-var PublicKey = require('./public-key.js')
-var PrivateKey = require('./private-key.js')
+var crypto = require('crypto')
 
 module.exports = function(configs) {
+	var keys, password
 
-	if (configs === undefined) {
-		var key = ursa.generatePrivateKey(2048, 65537)
-		this.privateKey = new PrivateKey(key.toPrivatePem('base64'))
-		this.publicKey = new PublicKey(key.toPublicPem('base64'))
+	if (!configs) {
+		keys = ursa.generatePrivateKey(2048, 65537)
 	} else {
-		if (configs.pair) {
-			var keypair_text_array = configs.pair.split('\r\n')
-			if (keypair_text_array.length !== 2) {
-				throw 'invalid keypair input'
+		if (configs.publicKey && !configs.privateKey) {
+			keys = ursa.createPublicKey(configs.publicKey, 'base64')
+		} else if (configs.privateKey) {
+			password = configs.password
+			if (typeof password !== 'string') {
+				throw 'password is required for private keys'
 			}
-			configs.privateKey = keypair_text_array[0]
-			configs.publicKey = keypair_text_array[1]
-		}
-		if (configs.privateKey) {
-			this.privateKey = new PrivateKey(configs.privateKey, configs.password)
-		}
-		if (configs.publicKey) {
-			this.publicKey = new PublicKey(configs.publicKey)
+			var decipher = crypto.createDecipher('aes256', password)
+			var decrypted = decipher.update(configs.privateKey, 'base64', 'utf8') + decipher.final('utf8')
+			keys = ursa.createPrivateKey(decrypted, undefined, 'base64')
 		}
 	}
 
-	var self = this
-	this.toString = function(password) {
-		return self.privateKey.toString(password) + '\r\n' + self.publicKey.toString()
+	ursa.assertKey(keys)
+
+	return {
+		sign: function(message) {
+			if (typeof message !== 'string') {
+				throw 'message must already be string to sign'
+			}
+			if (!ursa.isPrivateKey(keys)) {
+				throw 'private key not initialized correctly'
+			}
+			var signer = ursa.createSigner('sha256')
+			signer.update(message, 'utf8')
+			return signer.sign(keys, 'base64')
+		},
+		verify: function(message, signature) {
+			if (typeof message !== 'string') {
+				throw 'message must already be a string to verify signature'
+			}
+			if (!ursa.isKey(keys)) {
+				throw 'public key not initialized correctly'
+			}
+			var verifier = ursa.createVerifier('sha256')
+			verifier.update(message, 'utf8')
+			return verifier.verify(keys, signature, 'base64')
+		},
+		decrypt: function(message) {
+			if (!ursa.isPrivateKey(keys)) {
+				throw 'private key not initialized correctly'
+			}
+			return keys.decrypt(message, 'base64', 'utf8')
+		},
+		encrypt: function(message) {
+			if (typeof message !== 'string') {
+				throw 'message must already be a string to encrypt'
+			}
+			if (!ursa.isKey(keys)) {
+				throw 'public key not initialized correctly'
+			}
+			return keys.encrypt(message, 'utf8', 'base64')
+		},
+		exportPrivateKey: function(secret) {
+			if (typeof secret !== 'string') {
+				secret = password
+			}
+			if (typeof secret !== 'string') {
+				throw 'private key cannot be extracted unencrypted'
+			}
+			var cipher = crypto.createCipher('aes256', secret)
+			return cipher.update(keys.toPrivatePem('base64'), 'utf8', 'base64') + cipher.final('base64')
+		},
+		exportPublicKey: function() {
+			return keys.toPublicPem('base64')
+		}
 	}
 
 }
--- test/test.js
+++ test/test.js
@@ -7,16 +7,16 @@ var SimpleEncrypt = require('../')
 test('message signing', function(t) {
 	var simple = new SimpleEncrypt()
 	var message = 'dogecoins are hilarious'
-	var signature = simple.privateKey.sign(message)
-	t.true(simple.publicKey.verifySignature(message, signature))
+	var signature = simple.sign(message)
+	t.true(simple.verify(message, signature))
 	t.end()
 })
 
 test('message encryption', function(t) {
 	var simple = new SimpleEncrypt()
 	var message = 'dogecoins are hilarious'
-	var encrypted_message = simple.publicKey.encrypt(message)
-	t.equal(simple.privateKey.decrypt(encrypted_message), message)
+	var encrypted_message = simple.encrypt(message)
+	t.equal(simple.decrypt(encrypted_message), message)
 	t.end()
 })
 
@@ -25,11 +25,11 @@ test('saving the encrypted private/public key', function(t) {
 	var message = 'dogecoins are hilarious'
 	var password = 'mysupersecretpassword'
 
-	var encrypted_message = simple.publicKey.encrypt(message)
-	var stringified_secret_key = simple.privateKey.toString(password)
+	var encrypted_message = simple.encrypt(message)
+	var stringified_secret_key = simple.exportPrivateKey(password)
 
 	var new_simple = new SimpleEncrypt({ privateKey: stringified_secret_key, password: password })
-	var decrypted_message = new_simple.privateKey.decrypt(encrypted_message)
+	var decrypted_message = new_simple.decrypt(encrypted_message)
 
 	t.true(message === decrypted_message)
 	t.end()
@@ -41,36 +41,53 @@ test('testing a private/public key saved to disk', function(t) {
 	var password = 'mysupersecretpassword'
 	var message = 'dogecoins are hilarious'
 
-	var simple = new SimpleEncrypt({
-		publicKey: public_key_file.toString(),
+	var simple_secret = new SimpleEncrypt({
 		privateKey: private_key_file.toString(),
 		password: password
 	})
+	var signature = simple_secret.sign(message)
 
-	var signature = simple.privateKey.sign(message)
-	t.true(simple.publicKey.verifySignature(message, signature))
+	var simple_public = new SimpleEncrypt({
+		publicKey: public_key_file.toString()
+	})
+	t.true(simple_public.verify(message, signature))
 
-	var encrypted_message = simple.publicKey.encrypt(message)
-	t.equal(simple.privateKey.decrypt(encrypted_message), message)
+	var encrypted_message = simple_public.encrypt(message)
+	t.equal(simple_secret.decrypt(encrypted_message), message)
 
 	t.end()
 })
 
-test('testing a private/public key saved to disk as single file', function(t) {
-	var key_file = fs.readFileSync('./test-key.pair')
-	var password = 'mysupersecretpassword'
-	var message = 'dogecoins are hilarious'
+test('private key without password fails', function(t) {
+	var private_key_file = fs.readFileSync('./test-key.sec')
 
-	var simple = new SimpleEncrypt({
-		pair: key_file.toString(),
-		password: password
-	})
+	t.plan(1)
+	try {
+		new SimpleEncrypt({ privateKey: private_key_file.toString() })
+	} catch (e) {
+		t.ok(true, 'should throw exception')
+	}
+	t.end()
+})
 
-	var signature = simple.privateKey.sign(message)
-	t.true(simple.publicKey.verifySignature(message, signature))
+test('private key with wrong password fails', function(t) {
+	var private_key_file = fs.readFileSync('./test-key.sec')
 
-	var encrypted_message = simple.publicKey.encrypt(message)
-	t.equal(simple.privateKey.decrypt(encrypted_message), message)
+	t.plan(1)
+	try {
+		new SimpleEncrypt({ privateKey: private_key_file.toString(), password: 'the wrong password' })
+	} catch (e) {
+		t.ok(true, 'should throw exception')
+	}
+	t.end()
+})
 
+test('bad private key fails', function(t) {
+	t.plan(1)
+	try {
+		new SimpleEncrypt({ privateKey: 'not a real key', password: 'not a real password' })
+	} catch (e) {
+		t.ok(true, 'should throw exception')
+	}
 	t.end()
 })
